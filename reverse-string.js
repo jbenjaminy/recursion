6f2e1a1c1490369d8e93500ce43f9bf2a6332ce2
Alex Bang (MBP) <alexbang@hotmail.com>
2016-08-15 14:07:27 -0400

    Solves string reverse

--- reverse-string.js
+++ reverse-string.js
@@ -1,3 +1,18 @@
 'use strict';
 
 /*Exercise 3: Take a string as input, reverse the string, and return the new string. */
+
+let reverse = (string, string2) => {
+  string2 = !string2 ? '' : string2;
+
+  if (string.length == 0) return string2;
+
+  string2 = string2 + string[string.length - 1];
+
+  string = string.substring(0, string.length - 1);
+
+  return reverse(string, string2);
+
+  }
+
+  console.log(reverse('t'));
